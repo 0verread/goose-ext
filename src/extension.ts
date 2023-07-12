d7f8799c0341ca662b000951f88b30258420e510
sub0ne1 <iamsubhajit.d@gmail.com>
2023-07-12 00:03:55 -0400

    Refactor is working: Kinda + moved to gpt-3-turbo

--- src/extension.ts
+++ src/extension.ts
@@ -8,6 +8,7 @@ export async function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand('goose.explain', askGooseToExplain),
+		vscode.commands.registerCommand('goose.refactor', askGooseToRefactor),
 		vscode.commands.registerCommand('goose.optimize', askGooseToRefactor),
 		vscode.commands.registerCommand('goose.setApiKey', resetToken),
 		vscode.commands.registerCommand('goose.resetApiKey', resetToken),
@@ -19,6 +20,7 @@ export async function activate(context: vscode.ExtensionContext) {
 	// TODO: more structural. Better prompt.
 	async function askGooseToExplain() { await askGoose('Can you explain what this code does?'); }
 	async function askGooseToRefactor() { await askGoose('Please refactor the provided code and return the result code only'); }
+	async function askGooseToOptimize() { await askGoose('Please optimize the provided code and return the result code only'); }
 
 	// Used to reset API key
 	async function resetToken() {
